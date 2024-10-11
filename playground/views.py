7b95fb42af281f2f39c1f8c3663c3b1264c6b0ef
Mark Masenda <mdlmasenda@gmail.com>
2024-10-11 12:49:08 -0500

    First implementation of the vending machine models and added the WorkLog for citations

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def Homepage(request):
-    return render(request, 'WebpageCode.html', {'name': 'Mosh'})
+    return render(request, 'Home.html')
 
 def Test(request):
     return render(request, '2.html')
@@ -13,3 +13,6 @@ def Manufacturer(request):
 
 def User(request):
     return render(request, 'NewUser.html')
+
+def createVendingMachine(nickname, location):
+    pass

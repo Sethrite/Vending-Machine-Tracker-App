b3d3ad8abb17db0844bb8a5674b5c7d0098b67bd
Mark Masenda <mdlmasenda@gmail.com>
2024-10-25 15:33:38 -0500

    Implemented the functions for the Vending Machine setups

--- playground/setupVendingMachine.py
+++ playground/setupVendingMachine.py
@@ -1,24 +1,50 @@
 from playground.models import VendingMachine, SnackSpot
 
-snack_options = []
-electronic_options = []
-drink_options = []
-
-def GridLayout():
-    Layout = [(x,y) for x in range(5) for y in range(4)]
-    print(Layout)
-
 def SetupVending():
     VendingMachine.objects.create(nickname='Vending Machine 1', location = '1')
     VendingMachine.objects.create(nickname='Vending Machine 2', location = '2')
     VendingMachine.objects.create(nickname='Vending Machine 3', location = '3')
 
+    SnackPos()
+
+def vmObjects(vm: VendingMachine):
+    for row in range(4):
+         for col in range(5):
+            SnackSpot.objects.create(machine=vm, snack=VendingItems(vm), amount=8, row=row, col=col)
+
+def VendingItems(Vending):
+    snack_options = []
+    electronic_options = []
+    drink_options = []
+
+    if Vending == "vm1":
+        for snack in snack_options:
+            item = snack
+            snack_options.remove(snack)
+            return item
+
+    if Vending == "vm2":
+        for part in electronic_options:
+            item = part
+            electronic_options.remove(part)
+            return item
+
+    if Vending == "vm3":
+        for drink in drink_options:
+            item = drink
+            drink_options.remove(drink)
+            return item
+
 def SnackPos():
     vm1 = VendingMachine.objects.get(id=1)
     vm2 = VendingMachine.objects.get(id=2)
     vm3 = VendingMachine.objects.get(id=3)
-    SnackSpot.objects.create(machine=vm1, row=0, col=0)
+    vmObjects(vm1)
+    vmObjects(vm2)
+    vmObjects(vm3)
+    
 
 if __name__ == "__main__":
-    GridLayout()
+    vmObjects("vm1")
+    vmObjects("vm2")
 

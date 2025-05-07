c86f312fe8f54ed7413b2253ef7978f5eba388ed
Mark Masenda <mdlmasenda@gmail.com>
2025-05-07 16:09:54 -0500

    Updated database setup

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ pipenv install pyarduino django pillow
 # Set up Django settings
 export DJANGO_SETTINGS_MODULE=App.settings
 
+# Making and applying the migrations to the database
+pipenv run python manage.py makemigrations
+pipenv run python manage.py migrate
+
 # Setup for the database assets and relations, also setting up Serial Connections
 python manage.py shell < setupVendingMachine.py
 python manage.py shell < setupSerial.py
@@ -21,7 +25,7 @@ sleep 3
 
 echo "Decreasing snack counts periodically..."
 pipenv run python manage.py decrement_snacks &
-sleep 2
+sleep 3
 
 # Open the web app in the default browser
 echo "Opening browser..."
